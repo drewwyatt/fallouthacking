5854b914673b85632f1241a9cf9b5ffc86789765
Drew Wyatt <Drew.Wyatt@activenetwork.com>
2015-10-29 17:08:27 -0500

    working question loop

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 #! /usr/bin/env node
+var prompt = require('prompt');
+var q = require('q');
+// console.log(process.argv);
 
-console.log(process.argv);
+var answer = 'awesome';
+var guesses = [];
+function promptForGuess(deferred) {
+  deferred = (deferred === undefined) ? q.defer() : deferred;
+
+  prompt.start();
+  prompt.get('guess', function(err, response) {
+    guesses.push(response.guess);
+    if(response.guess === answer) {
+      deferred.resolve();
+    }  else {
+      promptForGuess(deferred);
+    }
+  });
+
+  return deferred.promise;
+}
+
+promptForGuess().then(function() { console.log('Done!', guesses); });
