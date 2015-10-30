261c1c011a8aba97cb9aef91ea1d0bb2202b8824
Drew Wyatt <Drew.Wyatt@activenetwork.com>
2015-10-29 20:54:49 -0500

    reorganized WordService

--- services/wordservice/index.js
+++ services/wordservice/index.js
@@ -3,83 +3,95 @@ var q = require('q');
 
 module.exports = new Words();
 
+/**
+ * Expected Options
+ * ------------------------
+ * { length: Number, count: Number }
+ */
+
 function Words() {
-  this.get = function(options, cb) {
-    this.readFileToArray(options)
-      .then(this.shuffleArray)
-      .then(this.filter)
-      .then(this.upperCase)
-      .then(function(w) {
-        cb(w.array);
-      })
-      .catch(function(e) {
-        cb(null, e);
-      });
-  }
+  this.get = _get;
 
-  this.filter = function(args) {
-    var deferred = q.defer();
+  this._filter = _filter;
+  this._readFileToArray = _readFileToArray;
+  this._shuffleArray = _shuffleArray;
+  this._upperCase = _upperCase;
+}
 
-    var count = 0;
-    var filtered = args.array.filter(function(w) {
-      if(w.length === args.length && count <= args.count) {
-        count++;
-        return true;
+function _get(options, cb) {
+  this._readFileToArray(options)
+    .then(this._shuffleArray)
+    .then(this._filter)
+    .then(this._upperCase)
+    .then(function(w) {
+      cb(w.array);
+    })
+    .catch(function(e) {
+      cb(null, e);
+    });
+}
+
+function _filter(args) {
+  var deferred = q.defer();
+
+  var count = 0;
+  var filtered = args.array.filter(function(w) {
+    if (w.length === args.length && count <= args.count) {
+      count++;
+      return true;
+    }
+  });
+  args.array = filtered;
+  deferred.resolve(args);
+
+  return deferred.promise;
+}
+
+function _readFileToArray(args) {
+  var deferred = q.defer();
+
+  try {
+    fs.readFile('enable1.txt', 'utf8', function(err, data) {
+      if (err) {
+        deferred.reject(err);
+      } else {
+        args.array = data.replace(/(\r)/gm, '').split('\n');
+        deferred.resolve(args);
       }
     });
-    args.array = filtered;
-    deferred.resolve(args);
-
-    return deferred.promise;
-  };
-
-  this.readFileToArray = function(args) {
-    var deferred = q.defer();
-
-    try {
-      fs.readFile('enable1.txt', 'utf8', function(err, data) {
-        if (err) {
-          deferred.reject(err);
-        } else {
-          args.array = data.replace(/(\r)/gm, '').split('\n');
-          deferred.resolve(args);
-        }
-      });
-    } catch (err) {
-      deferred.reject(err);
-    }
-
-    return deferred.promise;
-  };
-
-  this.upperCase = function(args) {
-    var deferred = q.defer();
-
-    for(var i=0; i<args.array.length; i++) {
-      args.array[i] = args.array[i].toUpperCase();
-    }
-
-    deferred.resolve(args);
-    return deferred.promise;
-  };
-
-  this.shuffleArray = function(args) {
-    var deferred = q.defer();
-
-    try {
-      for (var i = args.array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = args.array[i];
-        args.array[i] = args.array[j];
-        args.array[j] = temp;
-      }
-
-      deferred.resolve(args);
-    } catch (err) {
-      deferred.reject(err);
-    }
-
-    return deferred.promise;
+  } catch (err) {
+    deferred.reject(err);
   }
 
+  return deferred.promise;
+}
+
+function _shuffleArray(args) {
+  var deferred = q.defer();
+
+  try {
+    for (var i = args.array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = args.array[i];
+      args.array[i] = args.array[j];
+      args.array[j] = temp;
+    }
+
+    deferred.resolve(args);
+  } catch (err) {
+    deferred.reject(err);
+  }
+
+  return deferred.promise;
+}
+
+function _upperCase(args) {
+  var deferred = q.defer();
+
+  for (var i = 0; i < args.array.length; i++) {
+    args.array[i] = args.array[i].toUpperCase();
+  }
+
+  deferred.resolve(args);
+  return deferred.promise;
 }
